1c373ac7749b4b8efe6a68f7e1b5c6f4b77f3751
Matteo <matt.dag09@gmail.com>
2022-09-20 16:31:48 -0400

    commit

--- question2 - lab2.py
+++ question2 - lab2.py
@@ -1,6 +1,6 @@
-a = int(input('Enter the first number : '))
-q = int(input('Enter the second number : '))
-n = int(input('Enter the third number : '))
+a = int(input('Enter the first number : ')) #Ask for input
+q = int(input('Enter the second number : ')) #Ask for input
+n = int(input('Enter the third number : ')) #Ask for input
 
 multiplication = a
 answer = [a]
---
+++ question3 - lab2.py
@@ -0,0 +1,9 @@
+x = int(input('Enter a non-negative integer: ')) #Ask to enter integer
+reverse = 0 #Set default reverse
+
+while x != 0: #Do loop while the number entered is not 0
+    number = x  % 10 #Make a variable(number) and make it equal to the modulo of x / 10
+    x = int(x / 10)
+    reverse = reverse * 10 + number
+
+print("The reverse is: " + str(reverse)) #Print reverse
