3c948dd72e89a0892e4f327419b429eab9f324ab
Matteo <matt.dag09@gmail.com>
2022-09-22 08:47:27 -0400

    Removed useless lines

--- Lab 2 - Question 4.py
+++ Lab 2 - Question 4.py
@@ -1,5 +1,5 @@
 x = int(input('Enter a positive integer: ')) #Ask user for input
-#list = [x] #Create list
+
 if x == 1:
     print(x)
 elif x != 1:
@@ -12,10 +12,6 @@ while x != 1: #Do while x is not equal to 1
             print(int(x))
         elif x != 1:
          print(int(x), end = ", ")
-       # list.append(int(x)) #add to list
     elif x % 2 == 1: #if remainder of x is 1 then,
         x = 3*x + 1
         print(int(x), end = ", ")
-        #list.append(int(x), end = ", ") #add to list
-
-#print(str(list)) #print answer
