09466115a48d21dad0638ce0e2c5c2ab1cbac1e1
Matteo <matt.dag09@gmail.com>
2022-09-28 15:29:44 -0400

    Finished Q3 L3

--- lab3/Lab 3 - Question 3.py
+++ lab3/Lab 3 - Question 3.py
@@ -1,2 +1,24 @@
 import os
-os.system('cls' if os.name == 'nt' else 'clear')
+import random
+
+again = "yes"
+
+while again == "y" or again == "yes":
+    os.system('cls' if os.name == 'nt' else 'clear')
+    n = int(input("Enter a non-negative integer: "))
+    randomNumber = random.randint(0,n+1)
+    print("A random number has been generated!")
+
+    while 1 > 0:
+        guess = int(input("Enter your guess:  "))
+        if guess > randomNumber:
+            print("Your guess is too large!")
+        elif guess < randomNumber:
+            print("Your guess is too small!")
+        else:
+            print("You guessed it right!")
+            again = str(input("Would you like to play again? (Y/N): "))
+            again = str.lower(again)
+            break
+
+print("Thanks for playing!\n")
