3dc6a16ca41d04cf075bd2f0c36dbe59fcd5ed62
Matteo <matt.dag09@gmail.com>
2022-09-28 13:56:17 -0400

    Finished Q1 Lab3

--- lab3/Lab 3 - Question 1.py
+++ lab3/Lab 3 - Question 1.py
@@ -1,1 +1,40 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+a = int(input("Input the first integer: "))
+b = int(input("Input the second integer: "))
+
+print("Primes = ", end = "")
+primeNumbers = 0
+
+for i in range(a, b+1):
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0:
+            is_prime = False
+    if is_prime and i != 1:
+        primeNumbers += 1
+        print(i, end = " ")
+
+print()
+print("Number of primes = " + str(primeNumbers))
+
+
+
+
+# n = int(input('Enter a positive integer: '))
+# is_prime = True
+
+# for i in range(2, n):
+#     if n % i == 0:
+#         is_prime = False
+#         break
+
+# if is_prime:    # is_prime == True
+#     print('The number is prime', end = ', ')
+# else:
+#     print('The number is not prime', end = ', ')
+    
+# print('end of the program')
 
