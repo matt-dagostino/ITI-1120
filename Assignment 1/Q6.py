2666e4132e4e11a86b09908128b763a6543f0e2b
Matteo <matt.dag09@gmail.com>
2022-10-04 20:26:00 -0400

    Update Q6.py

--- Assignment 1/Q6.py
+++ Assignment 1/Q6.py
@@ -4,3 +4,31 @@
 
 import os #Import for next line to work
 os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
+
+'''
+n = int(input("Enter the amount of numbers would you like to input: ")) #Asks for number (n)
+numbers =[]
+
+for i in range(0,n):
+    num = (float(input(str(i+1) + ") Enter a number: ")))
+    numbers.append(num)
+'''
+n=10
+numbers =[82.34, 81.71,76.25, 76.25, 68.72, 61.21, 95.67, 70.58, 61.21, 55.74]
+#---------------------------- Calculate Average ------------------------------
+ans = 0
+for i in range(0,n):
+    ans = ans + numbers[i]
+ans = ans / n
+print("Average:",ans)
+
+#---------------------------- Calculate Modes ------------------------------
+from collections import Counter # Imports the counter function which counts 
+c = Counter(numbers) # Counts how many times the numbers are repeated in a list
+final_list = c.items
+print(c)
+mode = []
+for number, repeatedTimes in final_list:
+    if repeatedTimes > 1:
+        mode.append(number)
+
