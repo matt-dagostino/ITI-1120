11262de70f9ebc1bd78054a002d777272b7d3a2b
Matteo <matt.dag09@gmail.com>
2022-10-05 10:36:25 -0400

    Updates Descriptions

--- lab4/Lab 4 - Question 2.py
+++ lab4/Lab 4 - Question 2.py
@@ -1,23 +1,6 @@
 #------------------------------------------------
 # Lab 4 - Question 2 - Matteo Dagostino
 #------------------------------------------------
-'''2 – Write a program that receives a positive integer 𝑛 > 1. Then, it reads 𝑛 real numbers. Your
-program should find the two numbers among these numbers that have the minimum difference. In
-other words, find 𝑥 and 𝑦 in the array such that |𝑥 − 𝑦| is minimized. If there are more than one
-pair with minimum difference, print the one that has smaller numbers. Check samples for
-clarification.
-Sample 1:
-Input
-    5
-    56.17 61.15 14.56 13.21 29.92
-Output
-    13.21 14.56
-Sample 2:
-Input
-    7
-    19.98 20.01 20.02 18.96 20.20 20.21
-Output
-    20.01 20.02'''
 
 import os #Import for next line to work
 os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
--- lab4/Lab 4 - Question 3.py
+++ lab4/Lab 4 - Question 3.py
@@ -4,3 +4,24 @@
 
 import os #Import for next line to work
 os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
+'''3- Write a program that receives two strings and checks if they are anagram of each other. Two
+strings are anagram, if they contain exactly the same characters. Upper case and lower case letters
+are not the same in this problem. Check the samples for clarification.
+Sample 1:
+Input
+rah1im&*
+*hmi1&ra
+Output
+These strings are anagram.
+Sample 2:
+Input
+axy3y%3
+33ax%
+Output
+These strings are not anagram.
+Sample 3:
+Input
+bmjarA
+AmjArb
+Output
+These strings are not anagram.'''
