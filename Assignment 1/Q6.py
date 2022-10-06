8ffb47238e791beaa0ed13da59cced8480f26ba8
Matteo <matt.dag09@gmail.com>
2022-10-05 20:37:16 -0400

    Finished Q7 + added comments (6,7)

--- Assignment 1/Q6.py
+++ Assignment 1/Q6.py
@@ -7,36 +7,36 @@ os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
 
 
 n = int(input("Enter the amount of numbers would you like to input: ")) #Asks for number (n)
-numbers =[]
+numbers =[] # Create list for numbers inputed
 
-for i in range(0,n):
-    num = (float(input(str(i+1) + ") Enter a number: ")))
-    numbers.append(num)
+for i in range(0,n): #Loop until how many times the user wants
+    num = (float(input(str(i+1) + ") Enter a number: "))) #Ask to enter number, store it in 'num' variable
+    numbers.append(num) # Add num to the list 'numbers'
 
-print()
+print() # Print an empty line
 #---------------------------- Calculate Average ------------------------------
-ans = 0
-for i in range(0,n):
-    ans = ans + numbers[i]
-ans = ans / n
-print("Average:",ans)
+ans = 0 # Set answer to 0
+for i in range(0,n): # Loop for all the numbers inputted
+    ans = ans + numbers[i] #Calculate sum of numbers
+ans = ans / n  #Calculate average (sum/numbers)
+print("Average:",ans) #Print average
 
 #---------------------------- Calculate Mode ------------------------------
-from statistics import multimode
-mode = multimode(numbers)
-mode = sorted(mode)
-print("Mode: ", end = "")
-print(*mode, sep=", ")
+from statistics import multimode #Import multimode from statistics (easily find mode/multiple modes)
+mode = multimode(numbers) #Find mode in list
+mode = sorted(mode) #Sort the list (smallest to biggest)
+print("Mode: ", end = "") #Print mode title
+print(*mode, sep=", ") #Print the mode(s)
 
 #---------------------------- Calculate Median ------------------------------
-sorted_list = sorted(numbers)
-middle = n // 2
-if n % 2 == 0:
-    median = (sorted_list[middle] + sorted_list[middle-1])/2
-else:
-    median = sorted_list[middle]
-print("Median: " + format(median, '.3f'))
+sorted_list = sorted(numbers) #Sort the numbers list from smallest to biggest
+middle = n // 2 # Find the middle of the list
+if n % 2 == 0: #If the middle between two numbers
+    median = (sorted_list[middle] + sorted_list[middle-1])/2 #Find the median
+else: # If the middle is a number
+    median = sorted_list[middle] #Set the median to the middle number
+print("Median: " + format(median, '.3f')) #Print the median (rounded ".3f")
 
 #---------------------------- Calculate Range ------------------------------
-range = sorted_list[len(sorted_list)-1] - sorted_list[0]
-print("Range:", range)
+range = sorted_list[len(sorted_list)-1] - sorted_list[0] #Biggest number in list - smallest number, store the answer in variable called range
+print("Range:", range) #Print the range
--- Assignment 1/Q7.py
+++ Assignment 1/Q7.py
@@ -6,45 +6,40 @@ from operator import truediv
 import os #Import for next line to work
 os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
 
-a = [0.0, 1.0, 2.1, 1.45]
-b = [0.0, 2.2, 4.32, 3.3]
-c = [0.0, -1.0, 2.12, 1.1]
-d = [0.0, -4.058, 8.266, 3.999]
+a = [0.0, 1.0, 2.1, 1.45] #Defaulted values from an example from the assignment for easy testing
+b = [0.0, 2.2, 4.32, 3.3] #Defaulted values from an example from the assignment for easy testing
+c = [0.0, -1.0, 2.12, 1.1] #Defaulted values from an example from the assignment for easy testing
+d = [0.0, -4.058, 8.266, 3.999] #Defaulted values from an example from the assignment for easy testing
 
-for i in range(1,4):
-    a[i] = float(input("(x) Input a: "))
-    b[i] = float(input("(y) Input b: "))
-    c[i] = float(input("(z) Input c: "))
-    d[i] = float(input("Input d: "))
-    print("Input received! Formula " + str(i) + " = " + str(a[i]) + "x + " + str(b[i]) + "y + " + str(c[i]) + "z = " + str(d[i]))
+for i in range(1,4): #Loop 3 times
+    print(str(i) + ") Input the following information for ax + by + cz = d") #Print information for user
+    print() #Print empty line
+    a[i] = float(input("Input a: ")) #Ask for input
+    b[i] = float(input("Input b: ")) #Ask for input
+    c[i] = float(input("Input c: ")) #Ask for input
+    d[i] = float(input("Input d: ")) #Ask for input
+    print("Input received! ----> " + str(a[i]) + "x + " + str(b[i]) + "y + " + str(c[i]) + "z = " + str(d[i])) #Confirms the input received
+    print("-------------------------------------------") #Print divider line
 
-denominator = (a[1]*((b[2])*(c[3])-(b[1])*(c[2])))-(a[2]*((b[3])*(c[3])-(b[1])*(c[1])))-(a[3]*((b[1])*(c[2]-(b[2])*(c[1]))))
-x_top = (d[1]*((b[2])*(c[3])-(b[1])*(c[2])))-(d[2]*((b[3])*(c[3])-(b[1])*(c[1])))-(d[3]*((b[1])*(c[2]-(b[2])*(c[1]))))
+denominator = a[1]*b[2]*c[3]+b[1]*c[2]*a[3]+c[1]*a[2]*b[3]-(b[1]*a[2]*c[3]+a[1]*c[2]*b[3]+c[1]*b[2]*a[3]) #Find the denominator for the answer (Cramer's rule)
+x_top = d[1]*b[2]*c[3]+b[1]*c[2]*d[3]+c[1]*d[2]*b[3]-(b[1]*d[2]*c[3]+d[1]*c[2]*b[3]+c[1]*b[2]*d[3]) #Find the numerator for x (Cramer's rule)
+y_top = a[1]*d[2]*c[3]+d[1]*c[2]*a[3]+c[1]*a[2]*d[3]-(d[1]*a[2]*c[3]+a[1]*c[2]*d[3]+c[1]*d[2]*a[3]) #Find the numerator for y (Cramer's rule)
+z_top = a[1]*b[2]*d[3]+b[1]*d[2]*a[3]+d[1]*a[2]*b[3]-(b[1]*a[2]*d[3]+a[1]*d[2]*b[3]+d[1]*b[2]*a[3]) #Find the numerator for z (Cramer's rule)
 
+if x_top == y_top or x_top == z_top or y_top == z_top: #If any of the numerator values are equal to each other, then,
+    if a[2]/a[1] == b[2]/b[1] and b[2]/b[1] == c[2]/c[1] and d[2]/d[1] == c[2]/c[1] or a[3]/a[1] == b[3]/b[1] and b[3]/b[1] == c[3]/c[1] and d[3]/d[1] == c[3]/c[1] or a[3]/a[2] == b[3]/b[2] and b[3]/b[2] == c[3]/c[2] and d[3]/d[2] == c[3]/c[2]: #Check to see if there are infinite number of answers
+        print("There are infinite number of answers!") #Print infinite number of answers
+        print() #Print empty line
+    else: #If there arent infinite number of answers, then,
+        print("No answer found!") #Print no answer found
+        print() #Print empty line
+    exit() #Exit the program
 
-denominator = a[1]*b[2]*c[3]+b[1]*c[2]*a[3]+c[1]*a[2]*b[3]-(b[1]*a[2]*c[3]+a[1]*c[2]*b[3]+c[1]*b[2]*a[3])
-x_top = d[1]*b[2]*c[3]+b[1]*c[2]*d[3]+c[1]*d[2]*b[3]-(b[1]*d[2]*c[3]+d[1]*c[2]*b[3]+c[1]*b[2]*d[3])
-y_top = a[1]*d[2]*c[3]+d[1]*c[2]*a[3]+c[1]*a[2]*d[3]-(d[1]*a[2]*c[3]+a[1]*c[2]*d[3]+c[1]*d[2]*a[3])
-z_top = a[1]*b[2]*d[3]+b[1]*d[2]*a[3]+d[1]*a[2]*b[3]-(b[1]*a[2]*d[3]+a[1]*d[2]*b[3]+d[1]*b[2]*a[3])
-
-
-print("------- Answer -------")
-if x_top == y_top or x_top == z_top or y_top == z_top:
-    if a[2]/a[1] == b[2]/b[1] and b[2]/b[1] == c[2]/c[1] and d[2]/d[1] == c[2]/c[1] or a[3]/a[1] == b[3]/b[1] and b[3]/b[1] == c[3]/c[1] and d[3]/d[1] == c[3]/c[1] or a[3]/a[2] == b[3]/b[2] and b[3]/b[2] == c[3]/c[2] and d[3]/d[2] == c[3]/c[2]:
-        print("There are infinite number of answers!")
-        print()
-    else:
-        print("No answer found!")
-        print()
-    exit()
-
-
-
-if denominator != 0:
-    x = (x_top / denominator)
-    y = (y_top / denominator)
-    z = (z_top / denominator)
-    print("x = " + str(round(x,2)))
-    print("y = " + str(round(y,2)))
-    print("z = " + str(round(z,2)))
-print()
+if denominator != 0: #If x,y,z can be devided by a number (not 0), then,
+    x = (x_top / denominator) #Find x
+    y = (y_top / denominator) #Find y
+    z = (z_top / denominator) #Find z
+    print("x = " + str(round(x,2))) #Print answer (x)
+    print("y = " + str(round(y,2))) #Print answer (y)
+    print("z = " + str(round(z,2))) #Print answer (z)
+print() #Print empty line
