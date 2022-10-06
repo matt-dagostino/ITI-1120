5e0fb1f3da954a30baee2a3e5ee6b4f040fd9357
Matteo <matt.dag09@gmail.com>
2022-10-05 22:05:26 -0400

    Q9 - In progess (coding 10 now)

--- Assignment 1/Q9.py
+++ Assignment 1/Q9.py
@@ -2,5 +2,27 @@
 # Assignment 1 - Question 9 - Matteo Dagostino
 #------------------------------------------------
 
-import os #Import for next line to work
+import os
+import string #Import for next line to work
 os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
+'''
+string1 = str(input("Enter the first string: "))
+string2 = str(input("Enter the second string: "))
+'''
+
+
+string1 = "abdc"
+string2 = "nbpabcggaccbaxu"
+
+string1_array = []
+for letter in string1:
+    string1_array.append(letter)
+
+string2_array = []
+for letter in string2:
+    string2_array.append(letter)
+
+print(string1_array)
+print(string2_array)
+
+print(n for n in range(len(string2)) if string2.find(string1, n) == n)
