fdfa5a2f9b25c8e44ce36747193471b928d37f47
Matteo <matt.dag09@gmail.com>
2022-11-02 12:03:07 -0400

    Finished question 1

---
+++ lab7/Lab 7 - Question 2.py
@@ -0,0 +1,6 @@
+#------------------------------------------------
+# Lab 7 - Question 2 - Matteo Dagostino
+#------------------------------------------------
+
+import os
+os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
