649d819e3be2b0978d6411046c274b95569756ce
Matteo <matt.dag09@gmail.com>
2022-11-04 13:29:15 -0400

    Added midterm files for midterm 1

---
+++ Midterm 1 - 300287930/Matteo Dagostino - Midterm 1 - Question 3.py
@@ -0,0 +1,6 @@
+#------------------------------------------------
+# Midterm 1 - Question 3 - Matteo Dagostino
+#------------------------------------------------
+
+import os #Import for next line to work
+os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
---
+++ Midterm 1 - 300287930/Matteo Dagostino - Midterm 1 - Question 4.py
@@ -0,0 +1,6 @@
+#------------------------------------------------
+# Midterm 1 - Question 4 - Matteo Dagostino
+#------------------------------------------------
+
+import os #Import for next line to work
+os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
