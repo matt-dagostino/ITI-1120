e3bff89509e18aaaa8ecaca740b9e6b21721ef07
Matteo <matt.dag09@gmail.com>
2022-11-05 10:46:20 -0400

    Midterm

--- Midterm 1 - 300287930/Matteo Dagostino - Midterm 1 - Question 1.py
+++ Midterm 1 - 300287930/Matteo Dagostino - Midterm 1 - Question 1.py
@@ -4,3 +4,22 @@
 
 import os #Import for next line to work
 os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
+
+prime_list= []
+x = int(input("Enter a number bigger than 2: "))
+
+for i in range(2, 200):
+    prime = True
+    for j in range(2, i):
+        if i % j == 0 and i != j:
+            prime = False
+            break
+    if prime == True:
+        prime_list.append(i)
+
+for i in range (0, len(prime_list)):
+    for j in range (0, len(prime_list)):
+        if prime_list[i] + prime_list[j] == x:
+            print(str(x) + ' = ' + str(prime_list[i]) + " + " + str(prime_list[j]))
+            print()
+            exit()
--- Midterm 1 - 300287930/Matteo Dagostino - Midterm 1 - Question 2.py
+++ Midterm 1 - 300287930/Matteo Dagostino - Midterm 1 - Question 2.py
@@ -3,4 +3,49 @@
 #------------------------------------------------
 
 import os #Import for next line to work
-os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
+os.system("cls" if os.name == "nt" else "clear") # Clears the console
+A = {}
+B = {}
+
+
+
+for i in range(0, int(input("Enter the amount of midterm grades that would you like to add: "))):
+    name = str(input("Enter name: "))
+    grade = int(input("----> Enter grade: "))
+    A[name] = grade
+
+print("--------------------------------------")
+for i in range(0, int(input("Enter the amount of final grades that would you like to add: "))):
+    name = str(input("Enter name: "))
+    grade = int(input("----> Enter grade: "))
+    B[name] = grade
+
+
+A = { "Mike Armstrong" : 35, "Zoey James": 38, "Martin Max": 27, "Albert Wesker": 31 }
+B = { "Hannah Tan" : 51, "Zoey James": 46, "Martin Max": 55, "Albert Wesker": 40 }
+
+def combine(midterm, final):
+    endGrade = {}
+    for key, value in midterm.items():
+        for key2,value2 in final.items():
+            if key in final:
+                if key == key2:
+                    temp_grade = value + value2
+                    print(temp_grade)
+                    endGrade[key] = temp_grade
+                    break
+            else:
+                endGrade[key] = 0
+    for key2,value2 in final.items():
+            if key2 in midterm:
+                break
+            else:
+                endGrade[key] = value2
+            
+    return endGrade
+
+print(A, B)
+print("--------------------------------------")
+print(combine(A,B))
+print()
+        
