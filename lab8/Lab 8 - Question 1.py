3048bbb49034c4025c126d65ea1228bfc467ab18
Matteo <matt.dag09@gmail.com>
2022-11-09 10:30:37 -0500

    Finished Question 1

--- lab8/Lab 8 - Question 1.py
+++ lab8/Lab 8 - Question 1.py
@@ -1,41 +1,75 @@
 #------------------------------------------------
-# Lab 7 - Question 1 - Matteo Dagostino
+# Lab 8 - Question 2 - Matteo Dagostino
 #------------------------------------------------
 
 import os
 os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
 
-universal = set()
-A = set()
-B = set()
+""" A = [[12, 22, 33, 56, 11], 
+    [15, 10, 21, 9, 18],
+    [0,  0, 0, 0, 8],
+    [7,  0, 0, 0, 0],
+    [10, 1,2,3,6]]
 
-def get_sets(message):
-    temp = set()
-    x = int(input("Enter the amount of numbers that would you like to add to the " + message + " set: "))
-    for i in range(0,x):
-        temp.add(int(input(str(i+1) + ") Enter a number to add: ")))
-    return temp
+n = 4
+ """
 
-universal = get_sets("universal")
-A = get_sets("A")
-B = get_sets("B")
+A = []
+n = int(input("Enter the dimensions of the matrix (L x L): "))
+for i in range(0,n):
+    string = str(input(str(i+1) + ") Enter the line: "))
+    string = string.split()
+    A.append(string)
 
-print("------------------------------------------------------------")
-print("A union B: " + str(sorted(A.union(B))))
-print("A intersection B: " + str(sorted(A.intersection(B))))
-print("A - B: " + str(sorted(A.difference(B))))
-print("B - A: " + str(sorted(B.difference(A))))
-print("A Complement: " + str(sorted(universal.difference(A))))
-print("B Complement: " + str(sorted(universal.difference(B))))
-print("A ^ B: " + str(sorted(universal.difference(A).difference((universal.difference(B))).union((universal.difference(B)).difference((universal.difference(A)))))))
-if A.issubset(B):
-    if B.issubset(A):
-        print("A is a subset of B, and B is a subset of A")
-    else:
-        print("A is a subset of B, and B is not a subset of A")
-else:
-    if B.issubset(A):
-        print("A is not a subset of B, and B is a subset of A")
-    else:
-        print("A is not a subset of B, and B is not a subset of A")
-print("------------------------------------------------------------")
+""" 
+i = 0
+[i][n-1]
+[i][n-2]
+[i][n-3]
+i++ (1)
+[i][n-1]
+[i][n-2]
+i++(2)
+[i][n-1]
+ """
+lower = True
+for i in range(0,n-1): #0,1,2,3
+    for j in range(1,n-i): #1,2,3 -> #1,2 -> #1
+        if A[i][n-j] != "0":
+            lower = False
+            break
+
+
+""" 
+i = 0
+[i][n-1]
+[i][n-2]
+[i][n-3]
+i++ (1)
+[i][n-1]
+[i][n-2]
+i++(2)
+[i][n-1]
+ """
+upper = True
+for i in range(0,n-1): #0,1,2,3
+    for j in range(1,n-i): #1,2,3 -> #1,2 -> #1
+        if A[n-j][i] != "0":
+            upper = False
+            break
+
+
+
+print("------------------------ Output ------------------------")
+print('\n'.join([''.join(['{:5}'.format(item) for item in row]) for row in A]))
+print()
+
+if lower == False and upper == False:
+    print("This matrix is neither upper nor lower triangular.")
+elif lower == True and upper == True:
+    print("This matrix is both lower and upper triangular.")
+elif lower == True:
+    print("This matrix is lower triangular.")
+elif upper == True:
+    print("This matrix is upper triangular.")
+print()
--- lab8/Lab 8 - Question 2.py
+++ lab8/Lab 8 - Question 2.py
@@ -1,41 +1,6 @@
 #------------------------------------------------
-# Lab 7 - Question 1 - Matteo Dagostino
+# Lab 8 - Question 2 - Matteo Dagostino
 #------------------------------------------------
 
 import os
 os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
-
-universal = set()
-A = set()
-B = set()
-
-def get_sets(message):
-    temp = set()
-    x = int(input("Enter the amount of numbers that would you like to add to the " + message + " set: "))
-    for i in range(0,x):
-        temp.add(int(input(str(i+1) + ") Enter a number to add: ")))
-    return temp
-
-universal = get_sets("universal")
-A = get_sets("A")
-B = get_sets("B")
-
-print("------------------------------------------------------------")
-print("A union B: " + str(sorted(A.union(B))))
-print("A intersection B: " + str(sorted(A.intersection(B))))
-print("A - B: " + str(sorted(A.difference(B))))
-print("B - A: " + str(sorted(B.difference(A))))
-print("A Complement: " + str(sorted(universal.difference(A))))
-print("B Complement: " + str(sorted(universal.difference(B))))
-print("A ^ B: " + str(sorted(universal.difference(A).difference((universal.difference(B))).union((universal.difference(B)).difference((universal.difference(A)))))))
-if A.issubset(B):
-    if B.issubset(A):
-        print("A is a subset of B, and B is a subset of A")
-    else:
-        print("A is a subset of B, and B is not a subset of A")
-else:
-    if B.issubset(A):
-        print("A is not a subset of B, and B is a subset of A")
-    else:
-        print("A is not a subset of B, and B is not a subset of A")
-print("------------------------------------------------------------")
