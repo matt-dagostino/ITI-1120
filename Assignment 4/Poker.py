a2261485411703dbf92d5c8853c4b4e6496f0154
Matteo <matt.dag09@gmail.com>
2022-11-28 21:09:37 -0500

    Testing python

--- Assignment 4/Poker.py
+++ Assignment 4/Poker.py
@@ -10,7 +10,7 @@ os.system('cls' if os.name == 'nt' else 'clear') # Clears the console
 # Defining variables
 players = 2
 deck = []
-ranks = ["A", "2","3","4","5","6","7","8","9","T","J","Q","K",]
+ranks = ["A", "2","3","4","5","6","7","8","9","T","J","Q","K"]
 suits = ["D", "C", "S", "H"]
 
 for suit in suits:
@@ -18,7 +18,8 @@ for suit in suits:
         deck.append(rank+suit)
 
 class Poker:
-    def __init__(self):
+    def __init__(self, player):
+        self.playerName = player
         self.cards = []
         random.shuffle(deck)
 
@@ -30,6 +31,18 @@ class Poker:
         print(deck[0] + " has been placed on the table")
         deck.pop(0)
     
+    def IsFlush(self):
+        if "H" in self.cards[0] and "H" in self.cards[1] and "H" in self.cards[2] and "H" in self.cards[3] and "H" in self.cards[4]:
+            return True
+        elif "C" in self.cards[0] and "C" in self.cards[1] and "C" in self.cards[2] and "C" in self.cards[3] and "C" in self.cards[4]:
+            return True
+        elif "S" in self.cards[0] and "S" in self.cards[1] and "S" in self.cards[2] and "S" in self.cards[3] and "S" in self.cards[4]:
+            return True
+        elif "D" in self.cards[0] and "D" in self.cards[1] and "D" in self.cards[2] and "D" in self.cards[3] and "D" in self.cards[4]:
+            return True
+        else:
+            return False
+        
     def IsStraightFlush(self):
         if "H" in self.cards[0] and "H" in self.cards[1] and "H" in self.cards[2] and "H" in self.cards[3] and "H" in self.cards[4]:
             return True
@@ -44,13 +57,20 @@ class Poker:
 
     def show(self):
         print(self.cards)
+    
+    def set(self):
+        self.cards.append("1H")
+        self.cards.append("2H")
+        self.cards.append("3H")
+        self.cards.append("4H")
+        self.cards.append("5H")
 
-        
-p1 = Poker()
-p1.add_card(deck)
-p1.add_card(deck)
-p1.add_card(deck)
-p1.add_card(deck)
-p1.add_card(deck)
-print(p1.IsStraightFlush())
-p1.show()
+playerCards = []
+x = int(input("How many players are there? "))
+for i in range(0, x):
+    playerCards.append(Poker(i))
+
+    
+
+# if p1.IsFlush() == True:
+#     print("Flush")
